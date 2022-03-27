0bad4b5d8613f0673bf523f6e1e42601362d6a48
Dom Corvasce <dcorvasce@zohomail.eu>
2022-03-27 17:49:28 +0200

    chore: add sample app configuration file

---
+++ config.example.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+$_ENV['DB_USER'] = 'example';
+$_ENV['DB_PASSWD'] = 'example';
+$_ENV['DB_DRIVER'] = 'pgsql';
+$_ENV['DB_HOST'] = 'db';
+$_ENV['DB_PORT'] = 5432;
+$_ENV['DB_NAME'] = 'takeatoll';
