6c441eee75a74f48fb1ed5476334673a8b2adf62
Dom Corvasce <dcorvasce@zohomail.eu>
2022-03-27 20:35:12 +0200

    feat: add logic for computing monthly amount due for all customers

--- src/Jobs/CustomerBillingJob.php
+++ src/Jobs/CustomerBillingJob.php
@@ -4,6 +4,33 @@ declare(strict_types=1);
 
 namespace App\Jobs;
 
+use App\Models\Model;
+
 class CustomerBillingJob
 {
+    /**
+     * Computes the amount due for
+     *
+     * @param string $startDate The start date of the billing period
+     * @param string $endDate The end date of the billing period
+     * @return array The amount due of each customer for the whole billing period
+     */
+    public function execute(string $startDate, string $endDate): array
+    {
+        $query = <<<query
+            SELECT customers.id, SUM(passthroughs.cost) AS amountdue
+            FROM customers
+            RIGHT JOIN passthroughs ON passthroughs.customer_id = customers.id
+            WHERE passthroughs.updated_at BETWEEN ? AND ?
+            GROUP BY customers.id
+        query;
+
+        $model = new Model();
+        $statement = $model->getPDO()->prepare($query);
+        $statement->execute([$startDate, $endDate]);
+
+        // In a real world scenario, this data would be passed to another procedure
+        // which would take care of charging the customers for the amount due
+        return $statement->fetchAll();
+    }
 }
--- tests/Jobs/CustomerBillingJobTest.php
+++ tests/Jobs/CustomerBillingJobTest.php
@@ -2,9 +2,104 @@
 
 declare(strict_types=1);
 
+use App\Jobs\CustomerBillingJob;
+use App\Models\CustomerModel;
+use App\Models\TransponderModel;
+use App\Models\StationModel;
+use App\Models\PassthroughModel;
+use Faker\Factory;
 use PHPUnit\Framework\TestCase;
 
 class CustomerBillingJobTest extends TestCase
 {
+    // Stores the collection of fake customers
+    private array $customers;
 
+    // Stores the collection of fake stations
+    private array $stations;
+
+    // Stores the collection of fake transponders
+    private array $transponders;
+
+    /**
+     * Creates sets of fake customers and stations to use to record pass-throughs
+     *
+     * @return void
+     */
+    public function setUp(): void
+    {
+        $this->stations = [StationModel::fake(true), StationModel::fake(true)];
+        $this->customers = [CustomerModel::fake(true), CustomerModel::fake(true)];
+        $this->transponders = [
+            TransponderModel::fake(true, ['customer_id' => $this->customers[0]['id']]),
+            TransponderModel::fake(true, ['customer_id' => $this->customers[1]['id']]),
+        ];
+    }
+
+    /**
+     * Destroys the fake records used by the unit tests
+     *
+     * @return void
+     */
+    public function tearDown(): void
+    {
+        foreach ($this->transponders as $transponder)
+        {
+            TransponderModel::delete($transponder['serial_number']);
+        }
+
+        foreach ($this->customers as $customer)
+        {
+            CustomerModel::delete($customer['id']);
+        }
+
+        foreach ($this->stations as $station)
+        {
+            StationModel::delete($station['id']);
+        }
+    }
+
+    public function testComputingAmountsDue(): void
+    {
+        $faker = Factory::create();
+
+        // Create fake passthroughs
+        $passthroughs = [];
+        $expectedCost = 0.0;
+
+        for ($i = 0; $i < 10; $i += 1)
+        {
+            $cost = $faker->randomFloat(2, 1, 10);
+            $expectedCost += $cost;
+            $passthroughs[] = PassthroughModel::fake(true, [
+                'transponder_sn' => $this->transponders[0]['serial_number'],
+                'customer_id' => $this->customers[0]['id'],
+                'start_station_id' => $this->stations[0]['id'],
+                'end_station_id' => $this->stations[1]['id'],
+                // Even though the segment is always the same let's imagine we are dealing with
+                // different stations, hence different a different cost per segment
+                'cost' => $cost,
+                'created_at' => '2022-03-27 16:33:20',
+                'updated_at' => '2022-03-27 16:33:20',
+            ]);
+        }
+
+        // Executes job to compute amount due for a specific customer
+        $job = new CustomerBillingJob();
+        $billings = $job->execute('2022-03-21', '2022-03-28');
+
+        foreach ($billings as $billing)
+        {
+            if ($billing['id'] == $this->customers[0]['id']) {
+                $this->assertSame($expectedCost, (float) $billing['amountdue']);
+                break;
+            }
+        }
+
+        // Removes fake records
+        foreach ($passthroughs as $passthrough)
+        {
+            PassthroughModel::delete($passthrough['id']);
+        }
+    }
 }
